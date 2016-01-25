1aab7c15796eb25bbe0a7233d0d7de565b6644d7
An La <a.ngoc.la@gmail.com>
2016-01-25 13:11:12 -0800

    Quiz 1 - Question 3 Soln - edit

--- q3.js
+++ q3.js
@@ -3,14 +3,11 @@
 var majorCities = {BC: ["Vancouver", "Victoria", "Prince George"], AB: ["Edmonton", "Calgary"]};
 
 var printCities = function(obj){
-    var bcCities = obj.BC;
-    var bcSentence = "BC has 3 main cities: " + bcCities.join(", ");
-    console.log(bcSentence);
 
-    var abCities = obj.AB;
-    var abSentence = "AB has 2 main cities: " + abCities.join(", ");
-    console.log(abSentence);
-
+    for (var key in obj){
+        var currObj = obj[key];
+        console.log(key + " has " + currObj.length + " main cities: " + currObj.join(", "));
+    }
     return;
 };
 
